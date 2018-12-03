13b839a9eb889a44e303128ae6600a19a6a5f051
Thiago Cardoso <cardoso.thiago@outlook.com.br>
2018-12-03 12:49:55 -0200

    Unificação de script Scrapy/Pyspark

--- apider-Big_Data.py
+++ apider-Big_Data.py
@@ -1,5 +1,7 @@
+from pyspark import SparkContext, SparkConf
 import scrapy
 
+#scrapy que lê a página e retorna um arquivo .json com as palavras
 class MainSpider(scrapy.Spider):
     name ='main-spider'
     start_urls = ['http://quotes.toscrape.com/']
@@ -14,3 +16,19 @@ class MainSpider(scrapy.Spider):
             yield {
                 'text': text
             }
+#aplicação spark que importa o arquivo .json          
+conf = SparkConf().setAppName("WordCount")                          #cria o app Counter
+sc = SparkContext.getOrCreate()                                     #instancia SparkContext
+rdd = sc.textFile('<path-to-data>/bigdata.txt')                     #chama o arquivo .json do scrapy
+filter_empty_lines = contentRDD.filter(lambda x: len(x) > 0)        #elmina linhas em branco
+words = filter_empty_lines.flatMap(lambda x: x.split(' '))          #splita as palavras pelo espaço em branco
+
+#map-reduce que conta as palavras do arquivo .json
+wordcount = words.map(lambda x:(x,1)) \
+.reduceByKey(lambda x, y: x + y) \
+.map(lambda x: (x[1], x[0])).sortByKey(False)
+
+for word in wordcount.collect():
+    print(word)
+
+wordcount.saveAsTextFile("/home/thiago/Counter")
